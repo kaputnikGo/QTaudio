a8458b306a4dd97b06e1d2ef1d8cca60faae68f2
Minty Fresh <kaputnikGo@gmail.com>
2020-09-17 16:12:28 +1000

    init

---
+++ plugins/Example/example.cpp
@@ -0,0 +1,37 @@
+/*
+ * Copyright (C) 2020  Kaputnik Go
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; version 3.
+ *
+ * QTaudio is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <QDebug>
+#include <QAudioDeviceInfo>
+//#include <QtMultimedia>
+
+#include "example.h"
+
+Example::Example() {
+
+}
+
+void Example::speak() {
+    qDebug() << "hello world!";
+    qDebug() << "check audio output devices:";
+    // testing
+    const auto deviceInfos = QAudioDeviceInfo::availableDevices(QAudio::AudioOutput);
+    for (const QAudioDeviceInfo &deviceInfo : deviceInfos)
+      qDebug() << "Device name: " << deviceInfo.deviceName();
+
+      // QML logging only, not C++
+      // console.log("Device name: " + deviceInfo.deviceName());
+}
